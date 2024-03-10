f1b732988fcf4934a8623f2ab7f30c833064b958
bacngo123 <162684193+bacngo123@users.noreply.github.com>
2024-03-10 11:51:14 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,1 @@
+clear && cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/caotu2k5/v3dir/raw/main/v3.zip && unzip v3.zip && chmod +x * && mv mod login && login
